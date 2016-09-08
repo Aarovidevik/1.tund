d3196c6b5e6d045ec8d218e723eaea74bc8f5b7b
Aaro Videvik <videvik@greeny.cs.tlu.ee>
2016-09-08 13:11:45 +0300

    added my name

---
+++ first_file.php
@@ -0,0 +1,12 @@
+<?php
+  $firstName = "Aaro";
+  $lastName = "Videvik";
+  
+  
+  //echo $firstName;
+  //echo$lastName;
+  
+  //Kahe stringi liitmiseks kasutan punkti
+  echo $firstName." ".$lastName;
+  
+?>
